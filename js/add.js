7c5b41ea1aae53ca8df8a486a8799b54189b4487
Choi Goon-ho <caiguanhao@gmail.com>
2014-05-03 21:26:11 +0800

    add form;

--- js/add.js
+++ js/add.js
@@ -15,5 +15,43 @@
         }
       }
     });
+
+    if ($('#district_selector').length == 1) {
+      $.getJSON('/js/districts.tree.json', function(districts){
+        var province = $('<select />', { id: 'province', name: 'province', 'class': 'form_select' });
+        province.append('<option value="">请选择地区</option>');
+        $.each(districts, function(a, b){
+          province.append('<option value="' + a + '">' + a + '</option>');
+        });
+        province.change(function(){
+          $('option[value=""]', this).remove();
+          $(this).nextAll().remove();
+          var p = $(this).val();
+          var city = $('<select />', { id: 'city', name: 'city', 'class': 'form_select' });
+          $.each(districts[p], function(a, b){
+            city.append('<option value="' + a + '">' + a + '</option>');
+          });
+          city.data('province', p);
+          $('#district_selector').append(city);
+          city.change(function(){
+            $(this).nextAll().remove();
+            var p = $(this).data('province'), c = $(this).val();
+            if (districts[p][c] instanceof Array && districts[p][c].length > 0) {
+              var district = $('<select />', { id: 'district', name: 'district', 'class': 'form_select' });
+              $.each(districts[p][c], function(a, b){
+                district.append('<option value="' + b + '">' + b + '</option>');
+              });
+              $('#district_selector').append(district);
+            }
+          }).trigger('change');
+        });
+        $('#district_selector').append(province);
+      });
+    }
+
+    $('#sform').submit(function() {
+      $(this).find('.subm').val('发送中...').prop('disabled', true);
+    });
+
   });
 })(jQuery);
